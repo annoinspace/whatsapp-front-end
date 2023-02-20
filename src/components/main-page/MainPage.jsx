d80722b716ca4cad7d6d0f1ea6ead309e5342cfe
annoinspace <aneesahalmask@gmail.com>
2023-02-20 11:08:00 +0000

    basic structure addes, closed chats css created

---
+++ src/components/main-page/MainPage.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+import "./mainCss.css"
+import SingleChatSidebar from "./SingleChatSidebar"
+import { ListGroup } from "react-bootstrap"
+import SearchChat from "./SearchChat"
+
+export default function MainPage() {
+  return (
+    <div id="main-page-container" className="">
+      <div id="green-banner"></div>
+      <div id="main-page-wrapper" className="d-flex ">
+        <div id="left-side-wrapper">
+          <div id="my-profile">profile </div>
+          <SearchChat />
+          <ListGroup id="all-chats" className="">
+            <SingleChatSidebar />
+            <SingleChatSidebar />
+            <SingleChatSidebar />
+            <SingleChatSidebar />
+            <SingleChatSidebar />
+            <SingleChatSidebar />
+            <SingleChatSidebar />
+            <SingleChatSidebar />
+            <SingleChatSidebar />
+            <SingleChatSidebar />
+            <SingleChatSidebar />
+            <SingleChatSidebar />
+            <SingleChatSidebar />
+            <SingleChatSidebar />
+          </ListGroup>
+        </div>
+
+        <div id="open-chat" className="border">
+          openchat
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/main-page/SearchChat.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function SearchChat() {
+  return <div>SearchChat</div>
+}
